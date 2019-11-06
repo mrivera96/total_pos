1f5870838088efcbe109a97b46acbccebb7aa03b
mrivera96 <jylrivera96@gmail.com>
2019-11-06 15:31:11 -0600

    migrations

---
+++ database/migrations/2019_11_06_134447_create_branchs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBranchsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branchs', function (Blueprint $table) {
+            $table->increments('branch_id');
+			$table->string('branch_name', 45);
+			$table->string('description',45);
+			$table->date('branch_register_date');
+			$table->int('branch_register_number');
+			$table->string('branch_address',50);
+			$table->int('branch_open_hour');
+			$table->int('branch_close_hour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branchs');
+    }
+}
